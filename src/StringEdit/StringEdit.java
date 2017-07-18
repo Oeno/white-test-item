b508a322a0d2fa3eb8c12306c0fcc6ed67305791
Songyeol Lee <soye73@naver.com>
2017-07-18 10:43:45 +0900

    Print number of alphabets at the highest frequency

--- src/StringEdit/StringEdit.java
+++ src/StringEdit/StringEdit.java
@@ -14,6 +14,9 @@ public class StringEdit {
         int length = 0;
         Stack<String> words = new Stack();
         List<String> punctList = new ArrayList();
+        int[] alNum = new int[26];
+        Map<Character, Integer> alNumMap = new HashMap();
+
 
         // 구두점을 떼어 내기 위한 Pattern instance 생성
         Pattern p = Pattern.compile("\\p{Punct}");
@@ -27,7 +30,7 @@ public class StringEdit {
             // 구두점 분할 후, 단어를 stack에 저장
             Matcher m = p.matcher(word);
             if (m.find()) {
-                punctList.add(m.group());
+                punctList.add(m.group() + " ");
                 words.push(word.replace(m.group(), ""));
             } else {
                 punctList.add(" ");
@@ -35,9 +38,18 @@ public class StringEdit {
             }
         }
 
-        // 문장에 포함된 알파벳의 전체 수 계산
+        // 문장에 포함된 알파벳의 전체 수 및 개별 수 계산
         for (String word : words) {
+            for (char alphabet : word.toLowerCase().toCharArray())
+                alNum[alphabet - 97]++;
+
             length += word.length();
+        }
+
+        // 개별 수 Map에 저장
+        char idx = 'a';
+        for (int value : alNum) {
+            alNumMap.put(idx++, value);
         }
 
         // 단어 역순으로 출력
@@ -47,5 +59,33 @@ public class StringEdit {
         System.out.printf("\n");
 
         System.out.println("전체 수 : " + length);
+
+        // 알파벳 숫자를 최상위 빈도로 출력
+        int value = 0;
+        for (char alphabet : sortByValue(alNumMap)) {
+            value = alNumMap.get(alphabet);
+            if (value == 0)
+                break;
+
+            System.out.printf("%c: %d\n", alphabet, value);
+        }
+    }
+
+    public static List<Character> sortByValue(final Map<Character, Integer> map) {
+        List<Character> list = new ArrayList();
+        list.addAll(map.keySet());
+
+         Collections.sort(list, new Comparator<Character>() {
+             @SuppressWarnings("unchecked")
+             @Override
+             public int compare(Character c1, Character c2) {
+                Integer v1 = map.get(c1);
+                Integer v2 = map.get(c2);
+
+                return ((Comparable) v2).compareTo(v1);
+             }
+         });
+
+         return list;
     }
 }
